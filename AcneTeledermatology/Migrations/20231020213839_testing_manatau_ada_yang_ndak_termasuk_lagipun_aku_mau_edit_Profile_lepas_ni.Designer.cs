f3a988bee8ab0e4143f9998f13bb37a2a42cd991
WLRY <63553251+WeendyLRY@users.noreply.github.com>
2023-10-25 02:17:05 +0800

    FinalVersion1

--- AcneTeledermatology/Migrations/20231020213839_testing_manatau_ada_yang_ndak_termasuk_lagipun_aku_mau_edit_Profile_lepas_ni.Designer.cs
+++ AcneTeledermatology/Migrations/20231020213839_testing_manatau_ada_yang_ndak_termasuk_lagipun_aku_mau_edit_Profile_lepas_ni.Designer.cs
@@ -4,6 +4,7 @@ using AcneTeledermatology.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AcneTeledermatology.Migrations
 {
     [DbContext(typeof(AcneTeleContext))]
-    partial class AcneTeleContextModelSnapshot : ModelSnapshot
+    [Migration("20231020213839_testing_manatau_ada_yang_ndak_termasuk_lagipun_aku_mau_edit_Profile_lepas_ni")]
+    partial class testing_manatau_ada_yang_ndak_termasuk_lagipun_aku_mau_edit_Profile_lepas_ni
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AcneTeledermatology/Migrations/20231020213839_testing_manatau_ada_yang_ndak_termasuk_lagipun_aku_mau_edit_Profile_lepas_ni.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcneTeledermatology.Migrations
+{
+    /// <inheritdoc />
+    public partial class testing_manatau_ada_yang_ndak_termasuk_lagipun_aku_mau_edit_Profile_lepas_ni : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
